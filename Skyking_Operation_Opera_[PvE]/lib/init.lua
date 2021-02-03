79bc0aa5950b8d1276404b97519a2cb9e5a257e2
John Velegrakis <sv9rxw@hotmail.com>
2021-02-03 02:13:57 +0200

    Skyking Operation Opera: fixed lua scripts and added audio and briefing

--- Skyking_Operation_Opera_[PvE]/lib/init.lua
+++ Skyking_Operation_Opera_[PvE]/lib/init.lua
@@ -1,29 +1,54 @@
 AGL_MAX = 800  -- feet
 AGL_MAX_PASSED = false
+AA_ZONE_PENETRATED = false
 DETECTION_DELAY = 30 -- seconds
-DETECTION_USER_FLAG_NAME = "10"
+DETECTION_USER_FLAG_NAME = '10'
 
 BLUE_COMMAND_GROUP = nil
 BLUE_COMMAND_GROUP_NAME = "Cmd"
 
 ACTIVE_AIRCRAFTS = {} -- table of active aircrafts
+ACTIVE_AIRCRAFT_NAMES = {}
 AGL_THRESHOLD_ZONES = {'AGLThresholdZone'}
+
+function __FILE__() return debug.getinfo(2, 'S').source end
+function __LINE__() return debug.getinfo(2, 'l').currentline end
+function __FUNC__() return debug.getinfo(2, 'n').name end
+
+function LOG(txt) env.info("SKYKING -- " .. txt, false) end
+function MSG(txt) trigger.action.outText("SKYKING -- " .. txt, 10) end
 
 function activeRemove(u)
     local len = 0
     local temp = nil
+    local res = false
+    
     if ACTIVE_AIRCRAFTS ~= nil then
         len = #ACTIVE_AIRCRAFTS
         local i = 0
         for i=1,len do
             temp = ACTIVE_AIRCRAFTS[i]
             if u:getName() == temp:getName() then
-                table.remove(ACTIVE_AIRCRAFTS, i)
-                return true
+                table.remove(ACTIVE_AIRCRAFTS, i)         
+                res = true       
             end
         end
     end
-    return false
+
+    res = false    
+    if ACTIVE_AIRCRAFT_NAMES ~= nil then
+        len = #ACTIVE_AIRCRAFT_NAMES
+        local i = 0
+        local _name = nil
+        for i=1,len do
+            _name = ACTIVE_AIRCRAFT_NAMES[i]
+            if u:getName() == _name then
+                table.remove(ACTIVE_AIRCRAFT_NAMES, i)                
+                res = true
+            end
+        end
+    end
+    return res
 end
 
 function metersToFeet(m)
@@ -58,36 +83,91 @@ function getUnitAGLFeet(u)
     return _unitAltFeet - _landAltFeet
 end
 
+function unitIsInZone(u, zoneName)    
+    local _zone = trigger.misc.getZone(zoneName)
+
+    if _zone == nil then
+        LOG(__FUNC__() .. ": zone with name \'" .. zoneName .. "\' == nil")
+        return false
+    end
+
+    if (mist.utils.get2DDist(u:getPosition().p, _zone.point) < _zone.radius) then
+        return true
+    end    
+
+    return false
+end
 
 trigger.action.outText("- Gloval variables initialized", 10)
 
 Event_Handler = {}
 function Event_Handler:onEvent(event)
     local _unit = event.initiator
+    if _unit == nil then
+        LOG(__FUNC__() .. ": event.initiator == nil")
+        return
+    end
+
+    if _unit:getCategory() ~= Object.Category.UNIT then
+        LOG(__FUNC__() .. ": event.initiator category != UNIT. Omitting.")
+        return
+    end
+
+    if _unit:isExist() == false then
+        LOG(__FUNC__() .. ": _unit does not exist")
+        return
+    end
+
     local msg = ""
     local status = false
-
-    if event.id == world.event.S_EVENT_PLAYER_ENTER_UNIT then                
-        -- trigger.action.outTextFor_unit:getCoalition(), "Player " .. _unit:getPlayerName() .. " entered unit " .. _unit:getName() .. " at " .. tostring(event.time), 10)        
-        if _unit:getCoalition() == coalition.side.BLUE then
-            table.insert(ACTIVE_AIRCRAFTS, _unit)
-            msg = "Player " .. _unit:getPlayerName() .. " entered unit " .. _unit:getName() .. " at " .. tostring(event.time)
-            trigger.action.outTextForCoalition(coalition.side.BLUE, msg, 10)   
-            local num_active = #ACTIVE_AIRCRAFTS
-            trigger.action.outTextForCoalition(coalition.side.BLUE, tostring(num_active) .. " active aircrafts for BLUEFOR", 10)                     
+    if _unit:getCoalition() == coalition.side.BLUE then
+        local _playerName = _unit:getPlayerName()
+        if _playerName == nil then
+            LOG(__FUNC__() .. ": _unit is not a human player _unit:getPlayerName() == nil. Omitting.")
+            return
         end
-    elseif event.id == world.event.S_EVENT_PLAYER_LEAVE_UNIT then
-        if _unit:getCoalition() == coalition.side.BLUE then
+
+        if event.id == world.event.S_EVENT_BIRTH then            
+            MSG("S_EVENT_BIRTH " .. _playerName)
+        end
+
+        if event.id == world.event.S_EVENT_DEAD then
+            MSG("S_EVENT_DEAD " .. _playerName )
+        end
+
+        if event.id == world.event.S_EVENT_PILOT_DEAD then
+            MSG("S_EVENT_PILOT_DEAD " .. _playerName )
+        end
+
+        if event.id == world.event.S_EVENT_PLAYER_ENTER_UNIT then                            
+            table.insert(ACTIVE_AIRCRAFTS, _unit)
+            table.insert(ACTIVE_AIRCRAFT_NAMES, _unit:getName())
+            msg = "Player " .. _playerName .. " entered unit " .. _unit:getName() .. "\n"                       
+        elseif event.id == world.event.S_EVENT_PLAYER_LEAVE_UNIT then            
             status = activeRemove(_unit)
             if status == true then
-                msg = "Player " .. _unit:getPlayerName() .. " left unit " .. _unit:getName() .. " at " .. tostring(event.time)
+                msg = "Player " .. _playerName .. " left unit " .. _unit:getName() .. "\n"
             else
-               -- msg = "DEBUG: Internal Error: _unit not found in ACTIVE_AIRCRAFTS table"
+                LOG(__FUNC__() .. " S_EVENT_PLAYER_LEAVE_UNIT: _unit " .. _unit:getName() .. " not found in ACTIVE_AIRCRAFTS table")
             end
-            trigger.action.outTextForCoalition(coalition.side.BLUE, msg, 10)                        
-            local num_active = #ACTIVE_AIRCRAFTS
-            trigger.action.outTextForCoalition(coalition.side.BLUE, tostring(num_active) .. " active aircrafts for BLUEFOR", 10)
-        end     
+        elseif event.id == world.event.S_EVENT_UNIT_LOST then                                    
+            status = activeRemove(_unit)  
+            if status == true then
+                msg = "Player " .. _playerName .. " in unit " .. _unit:getName() .. " has been killed.\n"
+            else
+                LOG(__FUNC__() .. " S_EVENT_UNIT_LOST: _unit " .. _unit:getName() .. " not found in ACTIVE_AIRCRAFTS table")
+            end                          
+        elseif event.id == world.event.S_EVENT_CRASH then                        
+            status = activeRemove(_unit)  
+            if status == true then
+                msg = "Player " .. _playerName .. " in unit " .. _unit:getName() .. " crashed.\n"
+            else                
+                LOG(__FUNC__() .. " S_EVENT_CRASH: _unit " .. _unit:getName() .. " not found in ACTIVE_AIRCRAFTS table")
+            end                                  
+        end
+        local num_active = #ACTIVE_AIRCRAFTS
+        msg = msg .. tostring(num_active) .. " active aircrafts for BLUEFOR"
+        trigger.action.outTextForCoalition(coalition.side.BLUE, msg, 10)                 
     end
 end
 
@@ -128,7 +208,7 @@ function setAGLThershold(arg)
     end
 
     AGL_MAX = arg['Threshold']
-    msg = "Set new AGL threshold at " .. AGL_MAX .. " feet"
+    local msg = "Set new AGL threshold at " .. AGL_MAX .. " feet"
     trigger.action.outTextForCoalition(coalition.side.BLUE, msg, 10)
 end
 
@@ -136,14 +216,60 @@ function blueDetected(arg, time)
     if AGL_MAX_PASSED == true then
         trigger.action.setUserFlag("10", true)
     end
+    if AA_ZONE_PENETRATED == true then
+        trigger.action.setUserFlag("10", true)
+    end
+end
+
+function showDebugInfo(arg)
+    if arg == nil then
+        LOG(__FUNC__() .. ": Internal Error: arg == nil", 2)
+        return
+    end    
+
+    if arg['Mode'] == nil then
+        LOG(__FUNC__() .. ": Internal Error: arg['Mode'] == nil", 2)
+        return
+    end
+
+    local msg = ""
+    if arg['Mode'] == 'getUserFlag' then
+        msg = "User flags: \n"
+        local _flagVal = 0        
+        _flagVal = trigger.misc.getUserFlag('10')
+        msg = msg .. "Flag 10 = " .. tostring(_flagVal) .. "\n"
+        _flagVal = trigger.misc.getUserFlag('11')
+        msg = msg .. "Flag 11 = " .. tostring(_flagVal) .. "\n"
+        _flagVal = trigger.misc.getUserFlag('13')
+        msg = msg .. "Flag 13 = " .. tostring(_flagVal) .. "\n"         
+    elseif arg['Mode'] == 'setUserFlag' then
+        local _flagName = arg['Flag']
+        trigger.action.setUserFlag(_flagName, true)
+        msg = "Set user flag " .. _flagName .. " = true"
+    end
+
+    if arg['Mode'] == "printPlayers" then
+        local _playersTable = coalition.getPlayers(coalition.side.BLUE)
+        if _playersTable == nil then
+            msg = "coalition.getPlayers() == nil"
+        else
+            local i
+            local len = #_playersTable
+            msg = "Players:\n"
+            for i=1,len do
+                local _unit = _playersTable[i]
+                msg = msg .. _unit:getPlayerName() .. " in " .. _unit:getName() .. "\n"
+            end
+        end
+    end
+
+    trigger.action.outText(msg, 5)
 end
 
 world.addEventHandler(Event_Handler)
 trigger.action.outText("- Event handlers configured", 10)
 
-
 missionCommands.addCommandForCoalition(coalition.side.BLUE, 'Aircrafts Status', nil, showActiveAirplanes, {['Coalition'] = coalition.side.BLUE})
-
 
 menuSetAGLThreshold = missionCommands.addSubMenu('Set AGL Threshold...')
 missionCommands.addCommandForCoalition(coalition.side.BLUE, '200 feet', menuSetAGLThreshold, setAGLThershold, {['Threshold'] = 200})
@@ -153,4 +279,12 @@ missionCommands.addCommandForCoalition(coalition.side.BLUE, '800 feet', menuSetA
 missionCommands.addCommandForCoalition(coalition.side.BLUE, '1000 feet', menuSetAGLThreshold, setAGLThershold, {['Threshold'] = 1000})
 missionCommands.addCommandForCoalition(coalition.side.BLUE, '1500 feet', menuSetAGLThreshold, setAGLThershold, {['Threshold'] = 1500})
 missionCommands.addCommandForCoalition(coalition.side.BLUE, '2000 feet', menuSetAGLThreshold, setAGLThershold, {['Threshold'] = 2000})
+
+menuDebug = missionCommands.addSubMenu('DEBUG...')
+missionCommands.addCommandForCoalition(coalition.side.BLUE, 'Print players', menuDebug, showDebugInfo, {['Mode'] = 'printPlayers', ['Flag'] = '100'})
+missionCommands.addCommandForCoalition(coalition.side.BLUE, 'Print user flags', menuDebug, showDebugInfo, {['Mode'] = 'getUserFlag', ['Flag'] = '13'})
+missionCommands.addCommandForCoalition(coalition.side.BLUE, 'Set User Flag 10 ON', menuDebug, showDebugInfo, {['Mode'] = 'setUserFlag', ['Flag'] = '10'})
+missionCommands.addCommandForCoalition(coalition.side.BLUE, 'Set User Flag 11 ON', menuDebug, showDebugInfo, {['Mode'] = 'setUserFlag', ['Flag'] = '11'})
+missionCommands.addCommandForCoalition(coalition.side.BLUE, 'Set User Flag 13 ON', menuDebug, showDebugInfo, {['Mode'] = 'setUserFlag', ['Flag'] = '13'})
+
 
--- Skyking_Operation_Opera_[PvE]/lib/logic.lua
+++ Skyking_Operation_Opera_[PvE]/lib/logic.lua
@@ -1,63 +1,80 @@
+--[[
+    Check mission constraints. Is relevant only when airplanes
+    are in AGL threshold zone - outside Isreal and before target zone.
 
+]]
 
-local idx = 0
-local len = 0
+local _userFlagValue = trigger.misc.getUserFlag(DETECTION_USER_FLAG_NAME)
 
-len = #ACTIVE_AIRCRAFTS
+-- trigger.action.outText("User Flag 10 = " .. tostring(_userFlagValue), 1)
 
-local unitNamesToCheck = {}
-
-for idx=1,len do
-    local _unit = ACTIVE_AIRCRAFTS[idx]
-    if _unit ~= nil then
-        table.insert(unitNamesToCheck, _unit:getName())
-    end
-end
-
-if #unitNamesToCheck > 0 then
-    local uTable = mist.getUnitsInZones(unitNamesToCheck, {'AGLThresholdZone'})
-
-    len = #uTable
-
-    local any_unit_above_threshold = false
-
-    for idx=1,len do
-        local msg = ""
-        --local _unit = ACTIVE_AIRCRAFTS[idx]    
-        local _unit = uTable[idx]
-
-        if _unit ~= nil then                        
-            local _name = _unit:getPlayerName()
-            local _unitAGL = getUnitAGLFeet(_unit) 
-
-            -- Check if unit is in AGL Threshold Zone
+if _userFlagValue == 0 then
+    local idx = 0
+    local len = 0
             
-                    
-            if (_unitAGL  > AGL_MAX) then
-                local _userFlagValue = trigger.action.getUserFlag(DETECTION_USER_FLAG_NAME)
-                if _userFlagValue == false then
+    if #ACTIVE_AIRCRAFT_NAMES > 0 then
+        local uTable = mist.getUnitsInZones(ACTIVE_AIRCRAFT_NAMES, {'AGLThresholdZone'})
+    
+        len = #uTable
+    
+        -- Check if any airplane is above AGL threshold
+        local any_unit_above_threshold = false    
+        local any_unit_in_air_defence_zones = false
+        for idx=1,len do
+            local msg = ""
+            --local _unit = ACTIVE_AIRCRAFTS[idx]    
+            local _unit = uTable[idx]
+    
+            if _unit ~= nil then                        
+                local _name = _unit:getPlayerName()
+                local _unitAGL = getUnitAGLFeet(_unit) 
+    
+                -- Check if unit is above AGL threshold                                        
+                if (_unitAGL  > AGL_MAX) then                                        
                     if AGL_MAX_PASSED == false then                                            
                         -- schedule detection function
                         timer.scheduleFunction(blueDetected, {}, timer.getTime() + DETECTION_DELAY)
-                    end
+                    end                    
+    
+                    any_unit_above_threshold = true
+                    msg = "SKYKING. AGL WARNING: " .. _name .. " is ABOVE threshold " .. tostring(AGL_MAX) .. " feet at " .. tostring(_unitAGL) .. " feet."            
+                    trigger.action.outTextForCoalition(coalition.side.BLUE, msg, 1)
+                    -- msg = "DEBUG: land altitude at unit point = " .. tostring(_landAltFeet) .. " -> unit AGL = " .. tostring(_unitAltFeet - _landAltFeet) .. " feet"
+                    -- trigger.action.outTextForCoalition(coalition.side.BLUE, msg, 1)             
+                end        
+
+                -- Check if unit is in any air defence zone
+                if unitIsInZone(_unit, 'RedAispaceEast') == true then
+                    if AA_ZONE_PENETRATED == false then
+                        timer.scheduleFunction(blueDetected, {}, timer.getTime() + DETECTION_DELAY)
+                    end                                        
+                    any_unit_in_air_defence_zones = true
+                    msg = "SKYKING. COURSE WARNING: " .. _unit:getPlayerName() .. " you are very deep in eastern enemy air defence zone. Go back to your course."
+                    trigger.action.outTextForCoalition(coalition.side.BLUE, msg, 1)
+                    timer.scheduleFunction(blueDetected, {}, timer.getTime() + DETECTION_DELAY)    
+                end
+
+                if unitIsInZone(_unit, 'RedAispaceWest') == true then
+                    if AA_ZONE_PENETRATED == false then
+                        timer.scheduleFunction(blueDetected, {}, timer.getTime() + DETECTION_DELAY)
+                    end                                        
+                    any_unit_in_air_defence_zones = true
+                    msg = "SKYKING. COURSE WARNING: " .. _unit:getPlayerName() .. " you are very deep in western enemy air defence zone. Go back to your course."
+                    trigger.action.outTextForCoalition(coalition.side.BLUE, msg, 1)
+                    timer.scheduleFunction(blueDetected, {}, timer.getTime() + DETECTION_DELAY)    
                 end                
 
-                any_unit_above_threshold = true
-                msg = "SKYKING. AGL WARNING: " .. _name .. " is ABOVE threshold " .. tostring(AGL_MAX) .. " feet at " .. tostring(_unitAGL) .. " feet."            
-                trigger.action.outTextForCoalition(coalition.side.BLUE, msg, 1)
-                -- msg = "DEBUG: land altitude at unit point = " .. tostring(_landAltFeet) .. " -> unit AGL = " .. tostring(_unitAltFeet - _landAltFeet) .. " feet"
-                -- trigger.action.outTextForCoalition(coalition.side.BLUE, msg, 1)             
-            end        
-        else
-            msg = "DEBUG: Internal error: logic.lua: found unit == nil in ACTIVE_AIRCRAFTS at " .. tostring(idx)
-            trigger.action.outTextForCoalition(coalition.side.BLUE, msg, 10)
+            else
+                msg = "DEBUG: Internal error: " .. __FUNC__() .. ": found unit == nil in uTable at " .. tostring(idx)
+                LOG(msg)
+                MSG(msg)
+            end
         end
+    
+        AGL_MAX_PASSED = any_unit_above_threshold
+        AA_ZONE_PENETRATED = any_unit_in_air_defence_zones                       
     end
-
-    AGL_MAX_PASSED = any_unit_above_threshold
-
-end
-
--- Remove references for garbage collector
-unitNamesToCheck = nil
-uTable = nil
+    
+    -- Remove references for garbage collector
+    uTable = nil
+end    
