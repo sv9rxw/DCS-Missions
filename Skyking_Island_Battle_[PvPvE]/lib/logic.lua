2d392c8d8de5fdedfd343fb9eccf6cd5eba3b0ab
John Velegrakis <sv9rxw@hotmail.com>
2021-02-21 17:37:45 +0200

    Skyking Island Battle: -fixed lua messages. -fixed briefing text files. -removed heli units

--- Skyking_Island_Battle_[PvPvE]/lib/logic.lua
+++ Skyking_Island_Battle_[PvPvE]/lib/logic.lua
@@ -5,8 +5,8 @@ if not Group.getByName(BLUE_PREDATOR_GROUP_NAME) then
     if BLUE_PREDATOR_RESPAWN_TIMES_I < BLUE_PREDATOR_RESPAWN_TIMES_MAX then
         mist.respawnGroup(BLUE_PREDATOR_GROUP_NAME, true)
         BLUE_PREDATOR_RESPAWN_TIMES_I = BLUE_PREDATOR_RESPAWN_TIMES_I + 1        
-        msg = "SKYKING, SKYKING. INFO MESSAGE: AFAC Predator is has been taken down. New UAV Predator is airborne. " .. BLUE_PREDATOR_RESPAWN_TIMES_MAX - BLUE_PREDATOR_RESPAWN_TIMES_I .. " units remaining. Skyking, Skyking. Out."
-        trigger.action.outTextForCoalition(coalition.side.BLUE, msg, 30 , false)
+        msg = "SKYKING, SKYKING. INFO MESSAGE: Our AFAC Predator has been taken down. New UAV Predator is airborne. " .. BLUE_PREDATOR_RESPAWN_TIMES_MAX - BLUE_PREDATOR_RESPAWN_TIMES_I .. " units remaining. Skyking, Skyking. Out."
+        trigger.action.outTextForCoalition(coalition.side.BLUE, msg, 30)
     end
 end
 
@@ -14,8 +14,8 @@ if not Group.getByName(RED_PREDATOR_GROUP_NAME) then
     if RED_PRDATOR_RESPAWN_TIMES_I < RED_PREDATOR_RESPAWN_TIMES_MAX then
         mist.respawnGroup(RED_PREDATOR_GROUP_NAME, true)
         RED_PRDATOR_RESPAWN_TIMES_I = RED_PRDATOR_RESPAWN_TIMES_I + 1        
-        msg = "SKYKING, SKYKING. INFO MESSAGE: AFAC Predator is has been taken down. New UAV Predator is airborne. " .. RED_PREDATOR_RESPAWN_TIMES_MAX - RED_PRDATOR_RESPAWN_TIMES_I .. " units remaining. Skyking, Skyking. Out."
-        trigger.action.outTextForCoalition(coalition.side.RED, msg, 30 , false)
+        msg = "SKYKING, SKYKING. INFO MESSAGE: Our AFAC Predator has been taken down. New UAV Predator is airborne. " .. RED_PREDATOR_RESPAWN_TIMES_MAX - RED_PRDATOR_RESPAWN_TIMES_I .. " units remaining. Skyking, Skyking. Out."
+        trigger.action.outTextForCoalition(coalition.side.RED, msg, 30)
     end
 end
 ---------------------------------------------------------------------------
